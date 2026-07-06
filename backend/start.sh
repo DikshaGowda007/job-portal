6f52ea1f5a980101ccea766ded7bda1a6be30823
Diksha Gowda <dikshagowda2@gmail.com>
2026-07-06 15:44:45 +0530

    Add Render Docker deployment

---
+++ backend/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Starting Laravel..."
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+php artisan serve \
+    --host=0.0.0.0 \
+    --port=${PORT:-10000}
