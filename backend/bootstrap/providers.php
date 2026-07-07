dd3d93f781d2d9d36a05b8fd657f9ac1ecec8f36
Diksha Gowda <dikshagowda2@gmail.com>
2026-07-07 18:39:40 +0530

    telescope production issue fixed

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -1,9 +1,19 @@
 <?php
 
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\GateServiceProvider::class,
-    App\Providers\RepositoryServiceProvider::class,
-    App\Providers\RouteServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+use App\Providers\AppServiceProvider;
+use App\Providers\GateServiceProvider;
+use App\Providers\RepositoryServiceProvider;
+use App\Providers\RouteServiceProvider;
+
+$providers = [
+    AppServiceProvider::class,
+    GateServiceProvider::class,
+    RepositoryServiceProvider::class,
+    RouteServiceProvider::class,
 ];
+
+if (env('APP_ENV') === 'local') {
+    $providers[] = App\Providers\TelescopeServiceProvider::class;
+}
+
+return $providers;
