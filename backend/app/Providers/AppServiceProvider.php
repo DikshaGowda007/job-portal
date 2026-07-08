c02f65f652090f9835dce985f2e4041e42a317b2
Diksha Gowda <dikshagowda2@gmail.com>
2026-07-08 12:44:58 +0530

    schema changed

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+         DB::listen(function ($query) {
+            Log::info($query->sql, $query->bindings);
+        });
         Broadcast::routes(['middleware' => ['api', 'auth.broadcast']]);
 
         require base_path('routes/channels.php');
--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -9,28 +9,33 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name')->nullable();
-            $table->string('last_name')->nullable();
-            $table->string('email')->nullable();
-            $table->string('password')->nullable();
-            $table->unsignedTinyInteger('user_type')->default(4)->comment('1 - Admin, 2 - Sub Admin, 3 - Recruiter, 4 - Job Seeker');
-            $table->boolean('verified')->default(0)->comment('1 - verified, 0 - not verified');
-            $table->timestamp('last_login')->nullable();
-            $table->timestamp('created_at')->nullable();
-            $table->timestamp('updated_at')->nullable();
-            $table->unsignedTinyInteger('status')->default(1)->comment('1 - Active, 2 - Inactive');
-            $table->unsignedTinyInteger('is_deleted')->default(0)->comment('1 - Yes, 0 - No');
+
+            // $table->string('first_name')->nullable();
+            // $table->string('last_name')->nullable();
+            // $table->string('email')->nullable();
+            // $table->string('password')->nullable();
+
+            // $table->unsignedTinyInteger('user_type')->default(4);
+            // $table->boolean('verified')->default(false);
+
+            // $table->timestamp('last_login')->nullable();
+
+            // $table->timestamp('created_at')->nullable();
+            // $table->timestamp('updated_at')->nullable();
+
+            // $table->unsignedTinyInteger('status')->default(1);
+            // $table->unsignedTinyInteger('is_deleted')->default(0);
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
