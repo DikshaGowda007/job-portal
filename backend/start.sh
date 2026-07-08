f0bb6610495cdab0a4f4f80316a28bb582c13600
Diksha Gowda <dikshagowda2@gmail.com>
2026-07-08 12:19:48 +0530

    docker file updated

--- backend/start.sh
+++ backend/start.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
+set -e
 
 echo "Starting Laravel..."
 
+echo "Clearing config cache..."
+php artisan config:clear
+
+echo "Running migrations..."
+php artisan migrate --force -vvv
+
+echo "Caching configuration..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+echo "Starting server..."
 php artisan serve \
     --host=0.0.0.0 \
     --port=${PORT:-10000}
